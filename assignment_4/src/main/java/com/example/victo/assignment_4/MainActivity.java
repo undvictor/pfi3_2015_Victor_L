630f85b2c283b6676755d9db7b9b1f636c59721f
undvictor <undvictor@gmail.com>
2015-04-22 13:01:44 +0200

    assignment 4 - adapter implemented

--- assignment_4/src/main/java/com/example/victo/assignment_4/MainActivity.java
+++ assignment_4/src/main/java/com/example/victo/assignment_4/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.victo.assignment_4;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +18,24 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        PlanetGridFragment pGF = new PlanetGridFragment() ;
+        ft.replace(R.id.main_layout,pGF);
+        ft.commit();
+
+
+    /*    GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+    gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> parent, View v,
+                int position, long id) {
+            Toast.makeText(MainActivity.this, "" + position,
+                    Toast.LENGTH_SHORT).show();
+        }
+    });*/
     }
 
 
