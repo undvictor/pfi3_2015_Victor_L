d91ee94a6a518eebaeaccc5c5cfb13b77ea5d5a9
undvictor <undvictor@gmail.com>
2015-04-23 12:03:33 +0200

    assignment 4  - tinkering

--- assignment_4/src/main/java/com/example/victo/assignment_4/ImageAdapter.java
+++ assignment_4/src/main/java/com/example/victo/assignment_4/ImageAdapter.java
@@ -11,6 +11,7 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -39,6 +40,9 @@ public class ImageAdapter extends ArrayAdapter<Planet> {
         convertView = inflater.inflate(R.layout.list_item,null);
         ImageView iv = (ImageView) convertView.findViewById(R.id.iv_planet);
 
+        TextView tv = (TextView) convertView.findViewById(R.id.iv_text);
+
+        tv.setText(p.getName());
         iv.setImageDrawable(p.getImage());
         return convertView;
     }
